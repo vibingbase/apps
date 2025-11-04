d89109b200e2f7c74a264ea5e0faf0b55521fbe6
phuctm97 <phuctm97@gmail.com>
2025-11-04 21:48:25 +0700

    Handle more complex logic for dev workspace

--- setup-workspace.js
+++ setup-workspace.js
@@ -1,5 +1,37 @@
+import { existsSync, readFileSync, renameSync, writeFileSync } from "node:fs";
+import { execSync } from "node:child_process";
 import path from "node:path";
-import { existsSync, renameSync } from "node:fs";
+import YAML from "yaml";
+
+const pnpmWorkspaceFilePath = path.join(
+  process.env.VIBINGBASE_APP_DIR,
+  "pnpm-workspace.yaml",
+);
+
+const pnpmWorkspaceFileData = YAML.parse(
+  readFileSync(pnpmWorkspaceFilePath, "utf8"),
+);
 
 const workspaceDir = path.join(process.env.VIBINGBASE_APP_DIR, ".workspace");
-if (existsSync(workspaceDir)) renameSync(workspaceDir, "/workspace");
+
+if (existsSync(workspaceDir)) {
+  renameSync(workspaceDir, process.env.VIBINGBASE_WORKSPACE_DIR);
+  execSync("pnpm install", {
+    cwd: process.env.VIBINGBASE_WORKSPACE_DIR,
+    stdio: "inherit",
+  });
+  pnpmWorkspaceFileData.overrides = {
+    "@vibingbase/common-util": `link:${path.join(workspaceDir, "vibingbase-common-util")}`,
+    "@vibingbase/common-client": `link:${path.join(workspaceDir, "vibingbase-common-client")}`,
+    "@vibingbase/sdk": `link:${path.join(workspaceDir, "vibingbase-sdk")}`,
+  };
+} else {
+  delete pnpmWorkspaceFileData.overrides;
+}
+
+writeFileSync(pnpmWorkspaceFilePath, YAML.stringify(pnpmWorkspaceFileData));
+
+execSync(
+  "pnpm install --frozen-lockfile=false --fix-lockfile=true --lockfile-only=true --ignore-scripts=true",
+  { cwd: process.env.VIBINGBASE_APP_DIR, stdio: "inherit" },
+);
